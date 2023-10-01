46a7aa202e6db57568ee1cf6bbb5f4a67c3be7d5
Ray Liu <ray.rlius@gmail.com>
2023-10-01 16:33:40 +1100

    add modification

--- arraystringPractice.py
+++ arraystringPractice.py
@@ -46,12 +46,13 @@ class Solution:
         """
         total_len = m+n
         while m>0 and n>0:
-            if nums1[m-1] <= nums2[n-1]:
-                nums1[total_len-1] = nums2[n-1]
-                n -= 1
-            else:
+            if nums1[m-1] >=nums2[n-1]:
                 nums1[total_len-1] = nums1[m-1]
                 m -= 1
+            else:
+                nums1[total_len-1] = nums2[n-1]
+                n -= 1
+            
             total_len -= 1
         
         while n>0:
@@ -59,6 +60,7 @@ class Solution:
             n -= 1
 
 
+# test cases
 nums1 = [1,2,3,0,0,0]
 m = 3
 nums2 = [2,5,6]
